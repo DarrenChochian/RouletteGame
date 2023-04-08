e0208d7b4e9bc90a8964f36c5e216774b1d30dd7
DarrenChochian <dikran.d.c@outlook.com>
2023-04-08 15:59:54 -0700

    ALMOST DONE

--- src/console/RouletteGame.java
+++ src/console/RouletteGame.java
@@ -1,14 +1,14 @@
 package console;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Random;
 import java.util.Scanner;
+
 public class RouletteGame {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner (System.in);
+        Scanner input = new Scanner(System.in);
 
         System.out.println("Welcome to the Roulette Game!");
         System.out.println("The payout of winning bets are 1:1 for color bets and 35:1 for number bets.");
@@ -24,41 +24,51 @@ public class RouletteGame {
 
         System.out.println("Okay, " + name + " lets get ready to play some Roulette! \n");
         System.out.println("---------------------------");
-        
+
         ArrayList<String> colors = new ArrayList<>();
         colors.add("Red");
         colors.add("Black");
 
         for (String str : colors) {
-          System.out.println(str);
+            int bet = 0;
+            Random random = new Random();
+            int randomNumber = random.nextInt(37); 
 
-        
-        int randomNum = (int) Math.random();
-        
-        Random randomNum = new Random();
-        int randomNum = randomNum.nextInt(37);
-        System.out.println(randomNum);
+            while (true) {
+                System.out.println("Place your bets (The maximum you can bet is $50)\n");
+                bet = input.nextInt();
 
-        while(true) {
-            System.out.println("Place your bets (The maximum you can bet is $50)\n");
-
-            int bet = input.nextInt();
-
-            if(bet > 50) {
-                System.out.println("You cannot bet over $50");
+                if (bet > 50) {
+                    System.out.println("You cannot bet over $50");
+                    continue;
+                }
+                break;
             }
 
             System.out.println("Enter 1 to choose RED, Enter 2 to choose BLACK, or 3 to choose a number!(If you choose a number, it has to be between -1 and 36 inclusive)");
             int chosenNumber = input.nextInt();
 
-            if(chosenNumber == 1) {
+            if (chosenNumber == 1) {
                 System.out.println("Bets for this round:");
                 System.out.println("--------------------");
-                System.out.println("You chose the color Red, and bet $" + bet + " this round.");
+                System.out.println("You chose the color Red, and bet $" + bet + " this round.\n");
+                System.out.println("THE SPIN IS " + str + " " + randomNumber + "\n");
+                System.out.println("Results");
+                System.out.println("----------------");
+                if(str == "Red") {
+                	System.out.println("YOU WON! (you currently have $" + (startAmount + bet) + " left)\n");
+                }
+                else {
+                	System.out.println("You lost (you currently have " + (startAmount - bet) + " left)\n");
+                }
+                System.out.println("Do you want to play again (enter Y for yes and N for no)?");
+                String answer = input.nextLine();
+                if(answer.equalsIgnoreCase("N")) {
+                    break;
+                } else {
+                    answer = "";
+                }
+            } while(true);
             }
         }
-
     }
-
-}
-}

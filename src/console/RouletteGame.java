f7ea2ebc6dea2554bdccce595c7d4bd5615514f8
DarrenChochian <dikran.d.c@outlook.com>
2023-04-08 18:21:37 -0700

    adding even and odds with colors

--- src/console/RouletteGame.java
+++ src/console/RouletteGame.java
@@ -24,6 +24,22 @@ public class RouletteGame {
 
         System.out.println("Okay, " + name + " lets get ready to play some Roulette! \n");
         System.out.println("---------------------------");
+        
+        Random random = new Random();
+        int randomNumber = random.nextInt(38);
+        
+        String color = "";
+        
+        if (randomNumber == 0 || randomNumber == -1) {
+            color = "GREEN";
+        } else if (randomNumber % 2 == 0) {
+            color = "RED";
+        } else {
+            color = "BLACK";
+        }
+        
+        System.out.println("The wheel landed on " + color + " " + randomNumber);
+    
 
         ArrayList<String> colors = new ArrayList<>();
         colors.add("Red");
@@ -31,8 +47,8 @@ public class RouletteGame {
 
         for (String str : colors) {
             int bet = 0;
-            Random random = new Random();
-            int randomNumber = random.nextInt(37); 
+            Random random1 = new Random();
+            int randomNumber1 = random1.nextInt(37); 
 
             while (true) {
                 System.out.println("Place your bets (The maximum you can bet is $50)\n");
@@ -52,7 +68,7 @@ public class RouletteGame {
                 System.out.println("Bets for this round:");
                 System.out.println("--------------------");
                 System.out.println("You chose the color Red, and bet $" + bet + " this round.\n");
-                System.out.println("THE SPIN IS " + str + " " + randomNumber + "\n");
+                System.out.println("THE SPIN IS " + str + " " + randomNumber1 + "\n");
                 System.out.println("Results");
                 System.out.println("----------------");
                 if(str == "Red") {
@@ -61,6 +77,35 @@ public class RouletteGame {
                 else {
                 	System.out.println("You lost (you currently have " + (startAmount - bet) + " left)\n");
                 }
+                
+                if (chosenNumber == 2) {
+                    System.out.println("Bets for this round:");
+                    System.out.println("--------------------");
+                    System.out.println("You chose the color Red, and bet $" + bet + " this round.\n");
+                    System.out.println("THE SPIN IS " + str + " " + randomNumber1 + "\n");
+                    System.out.println("Results");
+                    System.out.println("----------------");
+                    if(str == "Black") {
+                    	System.out.println("YOU WON! (you currently have $" + (startAmount + bet) + " left)\n");
+                    }
+                    else {
+                    	System.out.println("You lost (you currently have " + (startAmount - bet) + " left)\n");
+                    }
+                    
+                if (chosenNumber == 3) {
+                    System.out.println("Bets for this round:");
+                    System.out.println("--------------------");
+                    System.out.println("You chose the color Red, and bet $" + bet + " this round.\n");
+                    System.out.println("THE SPIN IS " + str + " " + randomNumber1 + "\n");
+                    System.out.println("Results");
+                    System.out.println("----------------");
+                    if(str == "Red") {
+                    	System.out.println("YOU WON! (you currently have $" + (startAmount + bet) + " left)\n");
+                    }
+                    else {
+                    	System.out.println("You lost (you currently have " + (startAmount - bet) + " left)\n");
+                    }
+                
                 System.out.println("Do you want to play again (enter Y for yes and N for no)?");
                 String answer = input.nextLine();
                 if(answer.equalsIgnoreCase("N")) {
@@ -72,3 +117,5 @@ public class RouletteGame {
             }
         }
     }
+}
+}
